685d929006d3f908548db2248fa78ff0eedc876d
d-randoll <51640895+d-randoll@users.noreply.github.com>
2022-05-30 13:21:57 -0400

    added query for getting order

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersList.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersList
+    {
+        public class Query : IRequest<List<OrdersVm>>
+        {
+            public string UserName { get; set; }
+
+            public Query(string userName)
+            {
+                UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, List<OrdersVm>>
+        {
+            private readonly IOrderRepository _orderRepository;
+            private readonly IMapper _mapper;
+
+            public Handler(IOrderRepository orderRepository, IMapper mapper)
+            {
+                _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+                _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            }
+
+            public async Task<List<OrdersVm>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+                return _mapper.Map<List<OrdersVm>>(orderList);
+            }
+        }
+
+    }
+}
