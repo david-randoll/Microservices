69c447ef163868843ba49f838353d317c565e087
d-randoll <51640895+d-randoll@users.noreply.github.com>
2022-06-01 21:21:07 -0400

    configuring and adding ocelot api gateway

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
